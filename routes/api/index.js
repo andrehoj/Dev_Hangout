f198f85438eb27be07b3eff2fa90fc9c0bc76374
Eric Kohler <erickohler1600@gmail.com>
2022-05-26 20:54:04 -0400

    updated route endpoints

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
-router.use('/users', userRoutes);
+router.use('/signup', userRoutes);
+router.use('/login', userRoutes);
 
 module.exports = router;
